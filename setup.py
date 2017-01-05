9fa4d65deab76208b225a7ef70d9a9beb0572053
aleatzeni <alessia.atzeni.14@ucl.ac.uk>
2017-01-05 19:32:18 +0000

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name = "greengraph",
-    version = "0.1.0",
-    packages = find_packages(exclude=['*test']),
-	scripts=['greengraph.py'],
-    install_requires = ['argparse','matplotlib','geopy','numpy','requests']
-    )
